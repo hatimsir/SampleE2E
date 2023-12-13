913cb853a7b81f39e583609fc015effb1c511a04
hatimsir <hatimsir@gmail.com>
2023-12-13 12:15:53 +0530

    new changes

--- src/test/java/UIPages/GoogleHomePage.java
+++ src/test/java/UIPages/GoogleHomePage.java
@@ -1,20 +1,19 @@
 package UIPages;
 
 import net.serenitybdd.core.pages.PageObject;
+import net.thucydides.core.annotations.Step;
 import org.openqa.selenium.By;
 
 public class GoogleHomePage extends PageObject {
 
+    @Step
     public void isLogoDisplayed(){
         shouldBeVisible(By.xpath("//img[@class='lnXdpd']"));
     }
 
-    //This method currently not in use so commented
-    public String doSearch(String searchKey, String searchResult){
+    @Step
+    public void doSearch(String searchKey, String searchResult){
         typeInto($(By.xpath("//textarea[@title='Search']")),searchKey);
         waitForTextToAppear(searchResult);
-        clickOn($(By.xpath("//div[@aria-label='"+searchResult+"']")));
-        return find(By.xpath("//h3[text()='net.serenity-bdd']")).getText();
-        //hatim updated some code latest
     }
 }
---
+++ src/test/java/features/login/LoginCheck.feature
@@ -0,0 +1,6 @@
+Feature: Login to Google
+
+  Scenario: To open browser and check google page
+    Given User navigate to google page
+    When User see google page is opened
+    Then User check text box and close the browser
